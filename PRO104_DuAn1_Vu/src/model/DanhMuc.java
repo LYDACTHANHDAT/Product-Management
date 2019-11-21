8dce3651658769a6d0b115e4e03cfadcfce7010e
NoName <NoName@Administrator>
2019-11-21 15:07:02 +0700

    Insert data Vu

---
+++ PRO104_DuAn1_Vu/src/model/DanhMuc.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author ASUS
+ */
+public class DanhMuc {
+    private String maDM;
+    private String tenDM;
+
+    public DanhMuc() {
+    }
+
+    public DanhMuc(String maDM, String tenDM) {
+        this.maDM = maDM;
+        this.tenDM = tenDM;
+    }
+
+    public String getMaDM() {
+        return maDM;
+    }
+
+    public void setMaDM(String maDM) {
+        this.maDM = maDM;
+    }
+
+    public String getTenDM() {
+        return tenDM;
+    }
+
+    public void setTenDM(String tenDM) {
+        this.tenDM = tenDM;
+    }
+    
+    
+}
